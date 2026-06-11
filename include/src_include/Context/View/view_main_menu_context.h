638cdf83d5e31c3c53b6495a63cc5512be154f93
ElouanLeGavroche <d.elouan@laposte.net>
2026-06-11 19:45:58 +0200

    :construction: début du triangle (trop fatigué pour avancé

--- include/src_include/Context/View/view_main_menu_context.h
+++ include/src_include/Context/View/view_main_menu_context.h
@@ -2,6 +2,7 @@
 #define VIEW_MAIN_MENU
 
 #include <stdio.h>
+#include <string.h>
 
 #include "../../../../extern_lib/glad/glad.h"
 
@@ -11,7 +12,7 @@
 
 #include "../../../../include/src_include/Shared_tools/Transform_openGL_values/colors.h"
 
+void init_render(st_engine *engin_state);
 void update_render_main_menu(st_engine *engine_state);
-
 
 #endif
--- src/Context/View/view_main_menu_context.c
+++ src/Context/View/view_main_menu_context.c
@@ -1,20 +1,82 @@
 #include "../../../include/src_include/Context/View/view_main_menu_context.h"
 
-void update_render_main_menu(st_engine *engine_state){
-    
+
+void init_render(st_engine *engin_state){
+    const char *vertex_shader_source = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+    const char *fragment_shader_source = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
     glClearColor(num_to_01(123), num_to_01(12), num_to_01(123), 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
     float vertices[] = {
     -0.5f, -0.5f, 0.0f,
      0.5f, -0.5f, 0.0f,
      0.0f,  0.5f, 0.0f
     };  
     
-    unsigned int VBO;
+    // Tampon où l'on stock les sommets
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);  
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);  
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Création de l'objet shaders
+    unsigned int vertex_shader;
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+
+    unsigned int fragment_shader;
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+    glCompileShader(fragment_shader);
+
+    unsigned int shader_program;
+    shader_program = glCreateProgram();
+
+    // On lie le frag et le vert dans un seul prg
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+
+    // Une fois lié, l'on peux les supprimer
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+}
+void update_render_main_menu(st_engine *engine_state){
+    
+    
+    glUseProgram(shader_program);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+    glUseProgram(shader_program);
+    glBindVertexArray(VBO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
     
 }
