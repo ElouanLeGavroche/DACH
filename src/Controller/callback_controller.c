defd568b0b953166a65349df363204b3411db838
ElouanLeGavroche <d.elouan@laposte.net>
2026-07-05 16:47:45 +0200

    :poop: comme toujours, problèmes de merge, lol

--- src/Controller/callback_controller.c
+++ src/Controller/callback_controller.c
@@ -49,16 +49,7 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height){
       window_parametr->size_x = width;
       window_parametr->size_y = height;
 
-      // Calcule du ration pour la projection : 
-      camera->ratio = ((float)window_parametr->size_x / (float)window_parametr->size_y) * (4.0f / 3.0f);
 
-      glm_ortho(
-            -camera->ortho_size - camera->ratio,
-            camera->ortho_size + camera->ratio,
-            -camera->ortho_size,
-            camera->ortho_size, 
-            camera->near_z, 
-            camera->far_z, 
-            camera->projection
-      );
+      //resize_cam(data);
+
 }
