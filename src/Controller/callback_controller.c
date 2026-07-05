71c84fec20be1553f095ec43c925a08b095ce948
ElouanLeGavroche <d.elouan@laposte.net>
2026-07-05 16:50:03 +0200

    :twisted_rightwards_arrows:

--- src/Controller/callback_controller.c
+++ src/Controller/callback_controller.c
@@ -1,26 +1,5 @@
 #include "../../include/src_include/Controller/callback_controller.h"
 
-void resize_cam(st_window_user_data *data)
-{
-      st_loaded_windows_data *window_parametr = data->window;
-      st_camera *camera = data->camera;
-    //glFrustum(cx-halfWidth*aspect, cx+halfWidth*aspect, bottom, top, zNear, zFar);
-
-      // Calcule du ration pour la projection : 
-      //float aspect = ((float)window_parametr->size_x / (float)window_parametr->size_y);
-      float aspect = 1*(window_parametr->size_x / window_parametr->size_y);
-      float half_height = camera->ortho_size;
-      float half_widht = half_height * aspect;
-
-      glm_ortho(
-            half_widht , half_widht,
-            -camera->ortho_size, camera->ortho_size, 
-            -camera->near_z, 
-            camera->far_z, 
-            camera->projection
-      );
-      printf("%f, %f, %f\n", half_widht, half_height, camera->ortho_size);
-}
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
       st_window_user_data *data = glfwGetWindowUserPointer(window);
@@ -35,7 +14,14 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
       if(camera->ortho_size >= 25.0f)
             camera->ortho_size = 25.0f;
       
-      //resize_cam(data);
+      glm_ortho(
+            -camera->ortho_size - camera->ratio,
+            camera->ortho_size + camera->ratio,
+            -camera->ortho_size, camera->ortho_size, 
+            -100.0f, 
+            100.0f, 
+            camera->projection
+      );
       //glm_perspective(glm_rad(camera->ratio), (float)1280/(float)720, 0.1f, 100.0f, camera->projection);
 }
 
@@ -43,13 +29,23 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height){
 
       st_window_user_data *data = glfwGetWindowUserPointer(window);
       st_loaded_windows_data *window_parametr = data->window;
+      st_camera *camera = data->camera;
 
       glViewport(0, 0, width, height);
 
       window_parametr->size_x = width;
       window_parametr->size_y = height;
 
+      // Calcule du ration pour la projection : 
+      camera->ratio = ((float)window_parametr->size_x / (float)window_parametr->size_y) * (4.0f / 3.0f);
 
-      //resize_cam(data);
-
+      glm_ortho(
+            -camera->ortho_size - camera->ratio,
+            camera->ortho_size + camera->ratio,
+            -camera->ortho_size,
+            camera->ortho_size, 
+            camera->near_z, 
+            camera->far_z, 
+            camera->projection
+      );
 }
