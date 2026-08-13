03b5e157019aa4b2d2483bef62a22642882e43e3
ElouanLeGavroche <d.elouan@laposte.net>
2026-08-13 13:44:28 +0200

    :sparkles: le nouveau système de context est fonctionnel, mais pas encore parfait

--- src/Core/context_stack.c
+++ src/Core/context_stack.c
@@ -38,7 +38,7 @@ int exit_context(st_stack *stack)
         if(stack->level_of_depth > 1)
         {
             int i;
-            for(i = 0; i < stack->level_of_depth; i ++) stack->stack_context[i] = stack->stack_context[i + 1];
+            for(i = 1; i < stack->level_of_depth; i ++) stack->stack_context[i] = stack->stack_context[i + 1];
             stack->current_context = stack->stack_context[0];
 
             if(stack->current_context == NULL || stack->stack_context[0] == NULL)
