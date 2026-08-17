f66131db83eaf044c82394d729441248515937b1
ElouanLeGavroche <d.elouan@laposte.net>
2026-08-17 11:21:41 +0200

    :coffin:

--- src/Context/Model/model_pause_menu_context.c
+++ src/Context/Model/model_pause_menu_context.c
@@ -19,39 +19,4 @@ void update_logic_pause_menu(st_context *state)
         state->inputs.release[KEY_ESCAPE] = false; 
     }
 
-    // Déplacement de la caméra
-    if(state->inputs.down[KEY_Z] || state->inputs.pressed[KEY_Z])
-    {
-        move_camera(&state->render.camera, UP);
-    }
-    if(state->inputs.down[KEY_Q] || state->inputs.pressed[KEY_Q])
-    {
-        move_camera(&state->render.camera, LEFT);
-    }
-    if(state->inputs.down[KEY_S] || state->inputs.pressed[KEY_S])
-    {
-        move_camera(&state->render.camera, DOWN);
-    }
-    if(state->inputs.down[KEY_D] || state->inputs.pressed[KEY_D])
-    {
-        move_camera(&state->render.camera, RIGHT);
-    }
-    if(state->inputs.release[KEY_A])
-    {
-        //move_camera(&state->render.camera, ROTATE_L);
-        state->render.camera.target = (state->render.camera.target <= 0.0f)? 45.0f + state->render.camera.target: state->render.camera.target;
-        state->inputs.release[KEY_A] = false;
-    }
-    if(state->inputs.release[KEY_E])
-    {
-        //move_camera(&state->render.camera, ROTATE_R);
-        state->render.camera.target = (state->render.camera.target >= -0.0f)? -45.0f + state->render.camera.target: state->render.camera.target;
-        state->inputs.release[KEY_E] = false;
-    }
-    if(state->render.camera.target != 0.0f)
-    {
-        move_camera(&state->render.camera, (state->render.camera.target > 0.0f) ? ROTATE_L : ROTATE_R);
-    }
-
-    zoom_camera(&state->render.camera, &state->mouse);    
 }
