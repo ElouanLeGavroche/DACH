366c3bc41b315f3f2f685ebb4a73edcacedd7ffc
ElouanLeGavroche <d.elouan@laposte.net>
2026-08-20 11:47:08 +0200

    :bulb

--- src/Game/country.c
+++ src/Game/country.c
@@ -65,8 +65,8 @@ int get_indice(int x, int y, st_country *country)
         return -1;
     }
 
-    x = (x <= 0 && x >= country->size_x)? -1 : x;
-    y = (y <= 0 && x >= country->size_y)? -1 : y;
+    x = (x < country->min_x || x > country->size_x)? -1 : x;
+    y = (y < country->min_y || x > country->size_y)? -1 : y;
     
     return (x == -1 || y == -1)? -1 : ((x * country->size_x + 1) * y);
 }
@@ -106,12 +106,22 @@ st_country_tile* get_tile(int x, int y, st_country *country)
 
 st_country* better_load_map(const char *path)
 {
-   const char *err;
+    /* Déclaration des variables */
+    
+    // Permet de récupéré les erreur lié à Json
+    const char *err;
 
-    int temp_indice, i;
-    int max_x = 0, max_y = 0, total = 0;
+    // Variable temporaire ou d'indice
+    int temp_indice, i, j;
+    
+    // Variable min max pour définir des tailles de variables
+    int max_x = INT_MIN;
+    int max_y = INT_MIN;
+    int min_x = INT_MAX;
+    int min_y = INT_MAX; 
+    int total = 0;
 
-    // Structures des différents éléments stocké dans le JSON
+    // Variables Json
     struct json_object *root;
 
     struct json_object *blocks;
@@ -122,20 +132,26 @@ st_country* better_load_map(const char *path)
     struct json_object *_z;
     struct json_object *_angle;
 
-    int x, y, angle, type;
+    // Variable qui sera entrer dans les tiles
+    int x;
+    int y;
+    int angle;
+    int type;
     float z;
 
+    /* initialisation de country */
     st_country *country = malloc(sizeof(st_country));
     if(!country)
     {
         fprintf(stderr, "Allocation de country échouer : %s\n", strerror(errno));
         return NULL;
     }
-    country->size_x = 0;
-    country->size_y = 0;
-    country->min_x = 0;
-    country->min_y = 1;
+    country->size_x = INT_MIN;
+    country->size_y = INT_MIN;
+    country->min_x = INT_MAX;
+    country->min_y = INT_MAX;
 
+    /* Chargement du fichier */
     root = json_object_from_file(path);
     if(!root)
     {
@@ -144,6 +160,7 @@ st_country* better_load_map(const char *path)
         return NULL;
     }
 
+    /* Mettre les blocs dans une liste */
     json_object_object_get_ex(root, "blocks", &blocks);
     if(!blocks)
     {
@@ -152,10 +169,13 @@ st_country* better_load_map(const char *path)
         return NULL;
     }
 
+    /* Récuperer la taille de la liste */
     temp_indice = json_object_array_length(blocks);
     
+    /* Looper une première fois dans la liste pour définir les dimensions de la carte */
     for(i = 0; i < temp_indice; i ++)
     {
+        /* Récuperer un bloc */
         block = json_object_array_get_idx(blocks, i);
         if(!block)
         {
@@ -164,6 +184,7 @@ st_country* better_load_map(const char *path)
             return NULL;
         }
 
+        /* Récuperer ça valeur X */
         _x = json_object_object_get(block, "x");
         if(!_x)
         {
@@ -172,6 +193,7 @@ st_country* better_load_map(const char *path)
             return NULL;
         }
 
+        /* Récuperer ça valeur Y */
         _y = json_object_object_get(block, "y");
         if(!_y)
         {
@@ -179,15 +201,26 @@ st_country* better_load_map(const char *path)
             fprintf(stderr, "Erreur lors de la récupération de la pos y : %s\n", err);
             return NULL;
         }
+
+        /* Donner les valeur x et y à des varibles à type natif */
         x = json_object_get_int(_x);
         y = json_object_get_int(_y);
 
+        /* Vérification des min et max */
         if(x > max_x){ max_x = x; }
         if(y > max_y){ max_y = y; }
+
+        if(x < min_x){ min_x = x; }
+        if(y < min_y){ min_y = y; }
     }
     
+    /* Définir la taille de la carte */
+    /* Notez que l'on enregistre avec ça, des blocs qui n'existe pas : */
+    /*
+        Si un monde à des trou, le jeu le remplira avec des cases vide
+        qui existe d'un point de vue logique.
+    */
     total = max_x * max_y;
-    printf("Max x : %d \nMax y : %d\nTotal : %d\n", max_x, max_y, total);
 
     // Alloué à la map de quoi contenir tout les blocks
     country->tiles = malloc(sizeof(st_country_tile) * total);  
@@ -197,11 +230,19 @@ st_country* better_load_map(const char *path)
         return NULL;
     }
 
+    /* Enregistrer les données dans la structure */
     country->size_x = max_x;
     country->size_y = max_y;
 
+    country->min_x = min_x;
+    country->min_y = min_y;
+
+    /* à faire : boucle pour définir tout les blocs comme vide */
+
+    /* Seconde boucle pour crée les blocs qui existe */
     for(i = 0; i < temp_indice; i ++)
     {
+        /* On récupère un bloc */
         block = json_object_array_get_idx(blocks, i);
         if(!block)
         {
@@ -209,7 +250,8 @@ st_country* better_load_map(const char *path)
             fprintf(stderr, "Erreur lors de la récupération du bloc : %s\n", err);
             return NULL;
         }
-
+        
+        /* On récupère le type du bloc */
         _type = json_object_object_get(block, "t");
         if(!_type)
         {
@@ -218,6 +260,7 @@ st_country* better_load_map(const char *path)
             return NULL;
         }
 
+        /* On récupère ça coordonnée X */
         _x = json_object_object_get(block, "x");
         if(!_x)
         {
@@ -226,6 +269,7 @@ st_country* better_load_map(const char *path)
             return NULL;
         }
 
+        /* On récupère ça coordonnée y */
         _y = json_object_object_get(block, "y");
         if(!_y)
         {
@@ -234,6 +278,7 @@ st_country* better_load_map(const char *path)
             return NULL;
         }
 
+        /* On récupère ça coordonnée Z */
         _z = json_object_object_get(block, "z");
         if(!_z)
         {
@@ -242,6 +287,7 @@ st_country* better_load_map(const char *path)
             return NULL;
         }
 
+        /* On récupère sont angle (s'il est incliné vert rien/nord/sud/est...) */
         _angle = json_object_object_get(block, "angle");
         if(!_angle)
         {
@@ -250,27 +296,32 @@ st_country* better_load_map(const char *path)
             return NULL;
         }
 
+        /* On envoie ces valeur dans des variables typé natif */
         x = json_object_get_int(_x);
         y = json_object_get_int(_y);
+        /* Petit calcule pour que une hauteur de 1 soit à la bonne taille */
         z = (float)json_object_get_int(_z) / 2.5;
         angle = json_object_get_int(_angle);
         type = json_object_get_int(_type);
 
+        /* On envoie tout ça dans la structure */
         country->tiles[get_indice(x, y, country)].type = type;
         country->tiles[get_indice(x, y, country)].angled = angle;
         country->tiles[get_indice(x, y, country)].height = z;
-        printf("X : %d | Y : %d | Z : %f\n", x, y, z);
     }
-    printf("Taille x et y : %d %d\n", country->size_x, country->size_y);
+
+    /* Loop pour print le monde (debug)*/
     for(i = country->min_x; i <= country->size_x; i ++)
     {
-        for(y = country->min_y; y <= country->size_y; y ++)
+        for(j = country->min_y; j <= country->size_y; j ++)
         {
-            printf("x : %d | y : %d | res : %d\n", i, y, country->tiles[get_indice(i, y, country)].type);
+            printf("x : %d | y : %d | res : %d\n", i, j, country->tiles[get_indice(i, y, country)].type);
         }
         printf("\n");
         
     }
-    printf("Fin de func.\n");
+
+    /* Libération de la mémoire des éléments qui ne sont plus utiles*/
+    json_object_put(root);
     return country;
 }
